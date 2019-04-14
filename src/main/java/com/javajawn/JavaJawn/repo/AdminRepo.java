3119229946e37978bb27812c5d912e9eb7abe29d
rjsmall90 <rjsmall90@gmail.com>
2019-04-14 09:38:44 -0400

    nothing updtd

--- src/main/java/com/javajawn/JavaJawn/repo/AdminRepo.java
+++ src/main/java/com/javajawn/JavaJawn/repo/AdminRepo.java
@@ -2,7 +2,9 @@ package com.javajawn.JavaJawn.repo;
 
 import com.javajawn.JavaJawn.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdminRepo extends JpaRepository <Admin, Long> {
 
 }
