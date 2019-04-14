51558eb8461f291e06fea492f6eec2eac6673e32
rjsmall90 <rjsmall90@gmail.com>
2019-04-14 09:36:39 -0400

    admin and employee repos added

---
+++ src/main/java/com/javajawn/JavaJawn/repo/AdminRepo.java
@@ -0,0 +1,8 @@
+package com.javajawn.JavaJawn.repo;
+
+import com.javajawn.JavaJawn.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepo extends JpaRepository <Admin, Long> {
+
+}
---
+++ src/main/java/com/javajawn/JavaJawn/repo/EmployeeRepo.java
@@ -0,0 +1,8 @@
+package com.javajawn.JavaJawn.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository {
+}
